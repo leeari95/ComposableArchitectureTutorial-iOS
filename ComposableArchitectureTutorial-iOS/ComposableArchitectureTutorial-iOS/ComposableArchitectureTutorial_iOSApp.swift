5af1b075dcaf9e77acfac66a9483572d5926ddbb
Ari <lee_ari95@icloud.com>
2024-05-17 15:07:44 +0900

    feat: 빈 프로젝트 생성

---
+++ ComposableArchitectureTutorial-iOS/ComposableArchitectureTutorial-iOS/ComposableArchitectureTutorial_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableArchitectureTutorial_iOSApp.swift
+//  ComposableArchitectureTutorial-iOS
+//
+//  Created by Ari on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ComposableArchitectureTutorial_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
