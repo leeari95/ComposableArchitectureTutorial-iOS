c024751a59c2346766930533f6dfe07df2382e26
Ari <lee_ari95@icloud.com>
2024-05-21 08:04:51 +0900

    feat: Effect 테스트

--- ComposableArchitectureTutorial-iOS/ComposableArchitectureTutorial-iOSTests/ComposableArchitectureTutorial_iOSTests.swift
+++ ComposableArchitectureTutorial-iOS/ComposableArchitectureTutorial-iOSTests/ComposableArchitectureTutorial_iOSTests.swift
@@ -22,12 +22,24 @@ final class CounterFeatureTests: XCTestCase {
             $0.count = 0
         }
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testTimer() async {
+        let clock = TestClock() // 시간을 제어할 수 있도록 기능의 감속기에서 사용할 시계가 될 것
+        let store = TestStore(initialState: CounterFeature.State()) {
+            CounterFeature()
+        } withDependencies: { // 이를 위해 TestStore에 withDependencies라는 또 다른 후행 클로저를 제공하며, 이를 통해 원하는 종속성을 재정의할 수 있습니다.
+            $0.continuousClock = clock
+        }
+        await store.send(.toggleTimerButtonTapped) {
+            $0.isTimerRunning = true
+            
+        }
+        await clock.advance(by: .seconds(1)) //  timerTick 액션을 받기 전에 테스트 시계에 1초씩 진행하도록 지시합니다.
+        await store.receive(\.timerTick) {
+          $0.count = 1
+        }
+        await store.send(.toggleTimerButtonTapped) {
+          $0.isTimerRunning = false
         }
     }
-
 }
