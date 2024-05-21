7907ed763f19abf8d3439b3bdbbf2ac53bd19e1a
Ari <lee_ari95@icloud.com>
2024-05-21 09:01:30 +0900

    feat: 여러개의 Store를 제거하고 새롭게 구성된 AppFeature로 대체하기

--- ComposableArchitectureTutorial-iOS/ComposableArchitectureTutorial-iOS/AppFeature.swift
+++ ComposableArchitectureTutorial-iOS/ComposableArchitectureTutorial-iOS/AppFeature.swift
@@ -39,17 +39,19 @@ struct AppView: View {
     
     // 컴포저블 아키텍처에서는 여러 개의 독립된 스토어가 아닌 하나의 스토어에서 기능을 함께 구성하고 뷰를 제공하는 것을 선호합니다.
     // 이렇게 하면 기능이 서로 통신하기가 매우 쉬워지고 통신이 제대로 작동하는지 테스트도 작성할 수 있습니다.
-    let store1: StoreOf<CounterFeature>
-    let store2: StoreOf<CounterFeature>
+    let store: StoreOf<AppFeature>
     
     var body: some View {
         TabView {
-            CounterView(store: store1)
+            // Store에서 scope(state:action:) 메서드를 사용하여 tab1 도메인에만 초점을 맞춘 하위 store를 도출합니다.
+            // 이 작업은 key path 구문을 사용하여 state 필드와 action 열거형의 대/소문자를 구분하여 수행합니다.
+            
+            CounterView(store: store.scope(state: \.tab1, action: \.tab1))
                 .tabItem {
                     Text("Counter 1")
                 }
             
-            CounterView(store: store2)
+            CounterView(store: store.scope(state: \.tab2, action: \.tab2))
                 .tabItem {
                     Text("Counter 2")
                 }
