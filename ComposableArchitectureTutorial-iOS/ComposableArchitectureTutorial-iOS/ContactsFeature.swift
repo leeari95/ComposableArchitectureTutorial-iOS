1a218cc05ebf7ec4020bc8d96624ce47b8f53ba1
Ari <lee_ari95@icloud.com>
2024-05-23 15:28:53 +0900

    feat: 도메인과 모델링 개선
    - 현재 연락처 기능은 '연락처 추가' 시트 또는 삭제 알림의 두 가지 목적지로 이동할 수 있습니다.
    - 중요한 것은 한 번에 두 대상 모두로 이동할 수 없다는 것입니다.
    - 하지만 현재는 각 대상을 Presents()의 선택적 부분으로 표현하고 있기 때문에 가능합니다.
    - 탐색할 수 있는 기능을 표현하기 위해 옵션을 사용하면 유효하지 않은 상태의 수가 기하급수적으로 증가합니다.
    - 예를 들어, 2개의 옵션에는 1개의 유효하지 않은 상태가 있지만 3개의 옵션에는 4개의 유효하지 않은 상태가 있고, 4개의 옵션에는 11개의 유효하지 않은 상태가 있습니다.
    - 이렇게 부정확한 도메인 모델링은 어떤 기능이 표시되는지 정확히 알 수 없기 때문에 애플리케이션을 복잡하게 만듭니다.
    - 여러 목적지로 이동하기 위해 도메인을 보다 간결하게 모델링하는 방법을 살펴보겠습니다.

--- ComposableArchitectureTutorial-iOS/ComposableArchitectureTutorial-iOS/ContactsFeature.swift
+++ ComposableArchitectureTutorial-iOS/ComposableArchitectureTutorial-iOS/ContactsFeature.swift
@@ -19,14 +19,17 @@ struct Contact: Equatable, Identifiable {
 struct ContactsFeature {
     @ObservableState
     struct State: Equatable {
-        @Presents var addContact: AddContactFeature.State? // Presents() 매크로를 사용하여 옵셔널 값을 유지함으로써 Feature의 State를 함께 통합합니다.
-        @Presents var alert: AlertState<Action.Alert>?
+//        @Presents var addContact: AddContactFeature.State? // Presents() 매크로를 사용하여 옵셔널 값을 유지함으로써 Feature의 State를 함께 통합합니다.
+//        @Presents var alert: AlertState<Action.Alert>?
+        /// 두 개의 Presents() 옵션을 Destination.State를 가리키는 하나의 옵션으로 바꿉니다.
+        @Presents var destination: Destination.State?
         var contacts: IdentifiedArrayOf<Contact> = []
     }
     enum Action {
         case addButtonTapped
-        case addContact(PresentationAction<AddContactFeature.Action>) // 부모는 자식 Feature에서 보낸 모든 Action을 관찰할 수 있습니다.
-        case alert(PresentationAction<Alert>)
+//        case addContact(PresentationAction<AddContactFeature.Action>) // 부모는 자식 Feature에서 보낸 모든 Action을 관찰할 수 있습니다.
+//        case alert(PresentationAction<Alert>)
+        case destination(PresentationAction<Destination.Action>)
         case deleteButtonTapped(id: Contact.ID) // 연락처 목록의 행에서 삭제 버튼을 탭할 때 전송될 새 작업을 추가
         
         
@@ -34,50 +37,63 @@ struct ContactsFeature {
             case confirmDeletion(id: Contact.ID)
         }
     }
+    
+    /// ContactsFeature에서 탐색할 수 있는 모든 Feature에 대한 도메인과 로직을 보유한다.
+    @Reducer(state: .equatable) // Reducer(state:action:)의 state 인수를 사용하여 매크로에 Destination.State에 Equatable 적합성을 적용하도록 지시합니다.
+    enum Destination {
+        case addContact(AddContactFeature)
+        case alert(AlertState<ContactsFeature.Action.Alert>)
+    }
+    
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
             case .addButtonTapped:
                 // 연락처 목록 기능에서 '+' 버튼을 탭하면 이제 해당 Feature가 표시되어야 함을 나타내는 AddContactFeature를 채울 수 있습니다.
-                state.addContact = AddContactFeature.State(
-                  contact: Contact(id: UUID(), name: "")
+                state.destination = .addContact(
+                    AddContactFeature.State(
+                        contact: Contact(id: UUID(), name: "")
+                    )
                 )
                 return .none
 
             // AddContactFeature 내에서 'Save' 버튼을 탭하면 해당 Feature를 해제할 뿐만 아니라
             // 새 연락처를 ContactsFeature.State에 보관된 연락처 모음에 추가하고 싶습니다.
-            case let .addContact(.presented(.delegate(.saveContact(contact)))):
+            case let .destination(.presented(.addContact(.delegate(.saveContact(contact))))):
               state.contacts.append(contact)
               return .none
                 
-            case .addContact:
-              return .none
-                
-                
-            case let .alert(.presented(.confirmDeletion(id: id))):
+            case let .destination(.presented(.alert(.confirmDeletion(id: id)))):
               state.contacts.remove(id: id)
               return .none
-              
-            case .alert:
+
+            case .destination:
               return .none
 
             case let .deleteButtonTapped(id: id):
               // 삭제 버튼을 탭하면 사용자에게 연락처 삭제를 확인하도록 요청하기 위해 AlertState를 채울 수 있습니다.
-              state.alert = AlertState {
-                TextState("Are you sure?")
-              } actions: {
-                ButtonState(role: .destructive, action: .confirmDeletion(id: id)) {
-                  TextState("Delete")
-                }
-              }
-              return .none
+                state.destination = .alert(
+                    AlertState {
+                        TextState("Are you sure?")
+                    } actions: {
+                        ButtonState(role: .destructive, action: .confirmDeletion(id: id)) {
+                            TextState("Delete")
+                        }
+                    }
+                )
+                return .none
             }
         }
-        // 이렇게 하면 자식 Action이 시스템에 들어올 때 자식 Reducer를 실행하고 모든 Action에 대해 부모 Reducer를 실행하는 새 Reducer가 생성됩니다.
-        // 또한 자식 Feature가 해제될 때 effact 취소 등을 자동으로 처리합니다. 자세한 내용은 문서를 참조하세요.
-        .ifLet(\.$addContact, action: \.addContact) {
-          AddContactFeature()
-        }
-        .ifLet(\.$alert, action: \.alert) // Alert 로직을 ContactsFeature에 통합하기.
+//        // 이렇게 하면 자식 Action이 시스템에 들어올 때 자식 Reducer를 실행하고 모든 Action에 대해 부모 Reducer를 실행하는 새 Reducer가 생성됩니다.
+//        // 또한 자식 Feature가 해제될 때 effact 취소 등을 자동으로 처리합니다. 자세한 내용은 문서를 참조하세요.
+//        .ifLet(\.$addContact, action: \.addContact) {
+//          AddContactFeature()
+//        }
+//        .ifLet(\.$alert, action: \.alert) // Alert 로직을 ContactsFeature에 통합하기.
+        
+        /// 감속기 하단에 사용된 두 개의 ifLet을 Destination 도메인에 초점을 맞춘 하나의 ifLet으로 바꿉니다.
+        /// 이 표현식에서 Destination 유형을 지정할 필요도 없는데, 이는 Reducer() 매크로가 Destination 열거형에 적용된 방식에서 유추할 수 있기 때문입니다.
+        .ifLet(\.$destination, action: \.destination)
+
     }
 }
